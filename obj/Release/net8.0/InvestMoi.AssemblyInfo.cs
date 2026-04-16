31f8e418050459317ebb417089783abda95b1985
Moises David Gonzalez <xmenmoises@gmail.com>
2026-04-16 15:04:25 -0300

    ajuste css netlify

--- obj/Release/net8.0/InvestMoi.AssemblyInfo.cs
+++ obj/Release/net8.0/InvestMoi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvestMoi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783b4c0a15df16545314bd8742d902f2c976ce59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c22d8f3651140d9408082674eb57dc442e23d859")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvestMoi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvestMoi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
